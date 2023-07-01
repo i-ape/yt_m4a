e0594fe2ea8ba68ebdc254142c4f8849cd675b45
i0ape <125308834+i0ape@users.noreply.github.com>
2023-07-01 15:08:47 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use std::fs;
 use std::io::{self, Write};
 use std::process::Command;
 
-fn download_and_convert_to_m4a(youtube_link: &str, is_playlist: bool) {
+fn download_and_convert_to_m4a(youtube_link: &str, is_playlist: bool, audio_quality: u32) {
     let output_dir = "out";
     std::fs::create_dir_all(output_dir).expect("Failed to create output directory");
 
-    let mut command = Command::new("yt-dlp");
-    command.args([
+    let mut command = Command::new("yt-dlp");`jj
+    command.args(&[
         "-x",
         "--audio-format",
         "m4a",
         "--audio-quality",
-        "0",
+        &audio_quality.to_string(),
         "--add-metadata",
         "--metadata-from-title",
         "%(artist)s - %(title)s",
@@ -20,8 +20,22 @@ fn download_and_convert_to_m4a(youtube_link: &str, is_playlist: bool) {
     ]);
 
     if is_playlist {
-        command.args([
-            &format!("{}/%(playlist_index)s - %(title)s.%(ext)s", output_dir),
+        // Ask for the playlist name
+        print!("Enter the playlist name: ");
+        io::stdout().flush().unwrap();
+        let mut playlist_name = String::new();
+        io::stdin()
+            .read_line(&mut playlist_name)
+            .expect("Failed to read input");
+
+        let playlist_name = sanitize_directory_name(&playlist_name.trim());
+
+        let playlist_dir = format!("{}/{}", output_dir, playlist_name);
+        fs::create_dir_all(&playlist_dir).expect("Failed to create playlist directory");
+
+        // Update the command arguments with playlist name appended to file names
+        command.args(&[
+            &format!("{}/%(playlist_index)s - %(title)s.%(ext)s", playlist_dir),
             "--yes-playlist",
         ]);
     } else {
@@ -34,38 +48,12 @@ fn download_and_convert_to_m4a(youtube_link: &str, is_playlist: bool) {
 
     if command_output.status.success() {
         println!("Download and conversion complete!");
-
-        if is_playlist {
-            // Extract playlist name
-            let playlist_name = extract_playlist_name(youtube_link);
-            let playlist_dir = format!("{}/{}", output_dir, playlist_name);
-
-            // Create the playlist directory
-            fs::create_dir(&playlist_dir).expect("Failed to create playlist directory");
-
-            // Move downloaded files into the playlist directory and update metadata
-            move_files_to_directory(&playlist_dir);
-        }
     } else {
         eprintln!("Download and conversion failed!");
         if let Ok(stderr) = String::from_utf8(command_output.stderr) {
             eprintln!("yt-dlp error message:\n{}", stderr);
         }
     }
-}
-
-fn extract_playlist_name(youtube_link: &str) -> String {
-    let playlist_info_command = Command::new("yt-dlp")
-        .args(["--get-title", "--no-playlist", youtube_link])
-        .output()
-        .expect("Failed to execute yt-dlp command");
-
-    if playlist_info_command.status.success() {
-        let playlist_name = String::from_utf8_lossy(&playlist_info_command.stdout);
-        return sanitize_directory_name(&playlist_name);
-    }
-
-    "Unknown Playlist".to_owned()
 }
 
 fn sanitize_directory_name(name: &str) -> String {
@@ -75,25 +63,6 @@ fn sanitize_directory_name(name: &str) -> String {
         .filter(|&c| !invalid_chars.contains(&c))
         .collect::<String>();
     sanitized_name.trim().to_owned()
-}
-
-fn move_files_to_directory(target_directory: &str) {
-    let output_dir = "out";
-
-    let files = fs::read_dir(output_dir)
-        .expect("Failed to read output directory")
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.file_type().map(|t| t.is_file()).unwrap_or(false));
-
-    for file in files {
-        let file_name = file.file_name();
-        let file_path = file.path();
-
-        let new_file_path = format!("{}/{}", target_directory, file_name.to_string_lossy());
-
-        fs::rename(&file_path, &new_file_path)
-            .unwrap_or_else(|_| eprintln!("Failed to move file: {:?}", file_path));
-    }
 }
 
 fn main() {
@@ -109,5 +78,15 @@ fn main() {
 
     let is_playlist = youtube_link.contains("playlist");
 
-    download_and_convert_to_m4a(youtube_link, is_playlist);
+    // Ask for audio quality
+    let mut audio_quality_input = String::new();
+    print!("Enter audio quality (0 - best, 9 - worst): ");
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut audio_quality_input)
+        .expect("Failed to read input");
+
+    let audio_quality: u32 = audio_quality_input.trim().parse().unwrap_or(0);
+
+    download_and_convert_to_m4a(youtube_link, is_playlist, audio_quality);
 }
